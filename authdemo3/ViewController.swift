a7acdb77fa17a5b8f7128c00d2caf0201b169520
aarongasaway <aarongas1@gmail.com>
2020-08-19 03:18:55 -0400

    Initial Commit

---
+++ authdemo3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  authdemo3
+//
+//  Created by Aaron Gasaway on 8/19/20.
+//  Copyright © 2020 Aaron Gasaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
